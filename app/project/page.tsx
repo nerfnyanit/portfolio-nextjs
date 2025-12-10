164a7ffa59a84554ae8b616b19ae08cf431d519e
nerfnyanit <nerfnyanit@gmail.com>
2025-12-10 17:50:37 +0900

    FetchInputコンポーネントの追加とProjectのUX向上

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,23 +1,27 @@
 import Counter from "../components/Counter"
+import FetchInput from "../components/FetchInput"
 import NameInput from "../components/NameInput"
-
 
 export default function page() {
   return (
-   <>
-    <div className="fixed top-0 w-full text-center top-10">
-        <h1 className="text-4xl font-bold mb-2 ">Projectへ、ようこそ</h1>
-        <p className="text-lg font-semibold">ここでは私が作った作品が並べられています</p>
-    </div>
+    <>
+      <div className="absolute left-0 top-0 w-full text-center z-50 py-6 shadow">
+        <h1 className="text-4xl font-bold mb-2">Projectへ、ようこそ</h1>
+        <p className="text-lg font-semibold">
+          ここでは私が作った作品が並べられています
+        </p>
+      </div>
 
-    <main className="pt-40 text-center flex flex-col items-center">
-        <Counter label="one" step={1}/>
+      <main className="pt-40 text-center flex flex-col items-center">
+        <Counter label="one" step={1} />
         <hr className="w-3/4 border-t border-gray-300 my-6" />
         <NameInput />
         <hr className="w-3/4 border-t border-gray-300 my-6" />
+        <FetchInput />
+        <hr className="w-3/4 border-t border-gray-300 my-6" />
 
-      <h2 className="text-3xl font-semibold">追加予定</h2>
-    </main>
-   </>
-  )
+        <h2 className="text-3xl font-semibold mb-50">追加予定</h2>
+      </main>
+    </>
+  );
 }
